cc608a0d9b8031a25b031da36a6fd914e7bbca75
811922ctc <811922ctc@MA-CTC-INST-S05>
2023-03-13 13:55:06 -0400

    Add project files.

---
+++ RPG character roller/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPG_character_roller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RPGCharacter _character = new RPGCharacter();
+        private Random _rng = new Random();
+        public MainWindow()
+        { 
+            InitializeComponent();
+            _character.CharacterClass = CharacterClasses.Wizard;
+            updateStats();
+        }
+
+        private void ButtonUpdateName(object sender, RoutedEventArgs e)
+        {
+            _character.Name = textBoxName.Text;
+            NameBlock.Text = textBoxName.Text;
+        }
+
+        private void buttonReroll_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Roll();
+            updateStats();
+
+            double odds = .5;
+
+            _character.PartyMembers.Clear();
+
+            foreach (ListBoxItem i in listPotentialMembers.Items)
+            {
+                if ( _rng.NextDouble() < odds )
+                {
+                    RPGCharacter c = new RPGCharacter()
+                    {
+                        Name = i.Content.ToString()
+                    };
+                    _character.PartyMembers.Add(c);
+                }
+            }
+
+            listPartyMembers.Items.Clear();
+            foreach (RPGCharacter c in _character.PartyMembers)
+            {
+                ListBoxItem i = new ListBoxItem();
+                i.Content = $"{c.Name} STR: {c.Strength} INT: {c.Intelligence}";
+                listPartyMembers.Items.Add(i);
+            }
+            
+
+        }
+
+        private void updateStats()
+        {
+            textStrength.Text = _character.Strength.ToString();
+            textIntellegence.Text = _character.Intelligence.ToString();
+            textDexterity.Text = _character.Dexterity.ToString();
+            textStamina.Text = _character.Stamina.ToString();
+            textWisdom.Text = _character.Wisdom.ToString();
+            textCharisma.Text = _character.Charisma.ToString();
+        }
+
+    }
+}
